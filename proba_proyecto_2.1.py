4090fc10da553da4f06dde5b52c8493c830c0779
Felipe-V13 <68176209+Felipe-V13@users.noreply.github.com>
2023-04-03 17:44:12 -0600

    Add files via upload

--- proba_proyecto_2.1.py
+++ proba_proyecto_2.1.py
@@ -1,27 +1,13 @@
-
 import random
 
-# Definir variables para contar las caras y las cruces
-num_caras = 0
-num_cruces = 0
+# Largo/longitud de la secuencia
+seq_len = 10
 
-# Lanzar la moneda 10 veces y registrar los resultados
-i = 0
-while i < 10:
-    resultado = random.choice(['cara', 'cruz'])
-    if resultado == 'cara':
-        num_caras += 1
-    else:
-        num_cruces += 1
-    i += 1
+# Secuencia de los lanzamientos
+seq = []
+for i in range(seq_len):
+    moneda_fich = random.choice(['escudo', 'corona'])
+    seq.append(moneda_fich)
 
-#frecuencia
-# de
-# las salidas
-
-frecuencia_caras = num_caras / 10.0
-frecuencia_cruces = num_cruces / 10.0
-
-# Imprimir los resultados
-print("caras: ", num_caras)
-print("cruces ", num_cruces)
+# resultado
+print(seq)
